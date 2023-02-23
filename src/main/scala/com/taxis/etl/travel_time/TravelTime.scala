7c859f14d2fabbbe097b0bd6ae2a5cd31b367909
Santiago Ruiz <santiago.ruiz@kinesso.com>
2023-02-23 08:06:09 -0500

    The save method was updated

--- src/main/scala/com/taxis/etl/travel_time/TravelTime.scala
+++ src/main/scala/com/taxis/etl/travel_time/TravelTime.scala
@@ -1,17 +1,21 @@
 package com.taxis.etl.travel_time
 
 import com.taxis.etl.helpers.DataFrameUtils
+import com.taxis.etl.utils.Spark
+import org.apache.spark.sql.SaveMode
 import org.apache.spark.sql.functions.col
 
 object TravelTime {
 
   def main(args: Array[String]): Unit = {
+    Spark.init("myApp")
     getDataframes()
   }
 
   def getDataframes() = {
-    val newDF = DataFrameUtils.getDataframeFromParquet("src/main/resources/yellow_tripdata_2020-01.parquet")
+    val newDF = DataFrameUtils.getDataframeFromParquet("resources/yellow_tripdata_2020-01.parquet")
     val result = newDF.select(col("Trip_distance"))
-    result.write.csv("src/main/resources/results/result1.csv")
+    result.write.option("delimiter","\t").option("header","true").csv("results/csv")
+    result.write.format("parquet").mode(SaveMode.Overwrite).save("results/parquet")
   }
 }
